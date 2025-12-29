b473033c63b639b67273d9e1e25fd395732a0a90
Abolhassan Ashoori <ashoori.dev@gmail.com>
2025-12-29 23:38:47 +0330

    chore(dx): fix indent style & normalize files

--- src/store.ts
+++ src/store.ts
@@ -8,6 +8,10 @@ export const menuOpen = atom(false);
 export const colorMode = persistentAtom<ColorMode>("mode", "system");
 onSet(colorMode, ({ newValue }) => {
   const value =
-    newValue !== "system" ? newValue : window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    newValue !== "system"
+      ? newValue
+      : window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
   document.documentElement.dataset.theme = value;
 });
